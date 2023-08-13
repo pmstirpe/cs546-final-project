446e0fcb16ea3ef9d1d1bfa46cd560dda8c5b1e1
zdurbin123 <138169147+zdurbin123@users.noreply.github.com>
2023-08-13 13:42:21 -0400

    Update index.js
    adding new data files

--- data/index.js
+++ data/index.js
@@ -1,4 +1,13 @@
 //Here you will import data files and export them as used in previous labs
 import userDataFunctions from './users.js';
+import charityDataFunctions from './charities.js';
+import giftDataFunctions from './gifts.js';
+import reviewDataFunctions from './reviews.js';
+import commentDataFunctions from './comments.js';
 
 export const userData = userDataFunctions;
+export const charityData = charityDataFunctions;
+export const giftData = giftDataFunctions;
+export const reviewData = reviewDataFunctions;
+export const commentData = commentDataFunctions;
+
